89e12fc45737bd07dcf9809df3585b4ac9a5f9c4
RomaChernyak <118313101+RomaChernyak@users.noreply.github.com>
2023-07-23 23:48:16 +0300

    Update pixabay-api.jsx

--- src/Services/pixabay-api.jsx
+++ src/Services/pixabay-api.jsx
@@ -4,7 +4,7 @@ const Per_Page = 12;
 
 export function fetchGalleryImg(searchQuery, page) {
     return fetch(
-            `${BASE_URL}?q=${searchQuery}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${Per_Page}&page=${page}`
+        `${BASE_URL}?q=${searchQuery}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${Per_Page}&page=${page}`
     ).then(response => {
         if (response.ok) {
             return response.json();
